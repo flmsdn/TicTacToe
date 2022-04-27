7e95593ed6085f65e5e20a486cc8ef5e825507fe
DESKTOP-0SENO1Q\lumsd <flumsden@perse.co.uk>
2022-04-27 15:56:02 +0100

    Initial Changes

--- Game.py
+++ Game.py
@@ -1,18 +1,28 @@
 
 class Game:
 
+    EMPTY = " "
+    P1 = "0"
+    P2 = "X"
+
     def __init__(self):
-        pass
+        self.__board = [[Game.EMPTY for _ in range(3)] for _ in range(3)]
 
     def __repr__(self):
-        pass
+        s=""
+        for c in range(3):
+            s+=f"{c+1} | " + " ".join(self.__board[c]) +"\n"
+        return s
 
     def play(self,row,col):
-        pass
+        row -= 1
+        col -= 1
+        self.__board[row][col] = self.__player
+        self.__player = Game.P2 if self.__player == Game.P1 else Game.P1
     
     @property
     def winner(self):
-        pass
+        return None
 
 if __name__ == "__main__":
     pass
--- Ui.py
+++ Ui.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from Game import Game
 
 class Ui(ABC):
     '''Class to contain the UI'''
@@ -15,7 +16,15 @@ class Gui(Ui):
 
 class Terminal(Ui):
     def __init__(self):
-        pass
+        self.__game = Game()
+
+    def __getInput(self):
+        row, col = int(input("Enter Row: ")), int(input("Enter Column: "))
+        return row,col
 
     def run(self):
-        pass
+        while self.__game.winner == None:
+            print(self.__game)
+            row, col = self.__getInput()
+            self.__game.play(row, col)
+        print(f"The winner is {self.__game.winner}")
